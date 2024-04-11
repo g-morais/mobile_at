eb6d10367c6c2019972ce5a663df6d76477a7f6f
Guilherme Willam Morais <guilhermem750@gmail.com>
2024-04-11 18:37:08 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -16,3 +16,32 @@ self.addEventListener('install', event => {
     // Você pode adicionar lógica para responder à requisição aqui
   });
   
+  // service-worker.js
+
+const CACHE_NAME = 'my-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/main.js',
+  '/assets/img/favicon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
